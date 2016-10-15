845f487e6d9084ee9d40b5caa48083186ccde9f9
Kevin <tig3r86@gmail.com>
2016-10-15 15:46:54 +1100

    setup hacktimer to avoid browser throttling

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// this is an overide to the setInterval method to stop chrome from slowing down the call to interval when the user navigate to another tab. the script basically use webworkers api to run the setInterval from my brief look at the source
+import 'hacktimer'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createHashHistory, useBasename } from 'history'
