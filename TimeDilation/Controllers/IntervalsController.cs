1ce2d8d15db76a162bdc5963ae8d48a373e72a0e
Stokesbook <bramleyt@stridetech.com.au>
2016-10-15 12:06:59 +1100

    comments on parameter fields

--- TimeDilation/Controllers/IntervalsController.cs
+++ TimeDilation/Controllers/IntervalsController.cs
@@ -23,26 +23,52 @@ namespace TimeDilation.Controllers
     }
     public class IntervalTiming
     {
+        /// <summary>
+        /// For staggered offsets. In the basic case, this will be 0.
+        /// Values in ms
+        /// </summary>
+        public long OffsetFromStartTime { get; set; }
+        /// <summary>
+        /// values in ms
+        /// </summary>
         public long ExpectedDuration { get; set; }
+        /// <summary>
+        /// values in ms
+        /// </summary>
         public long ActualDuration { get; set; }
     }
 
 
     public class ApiResult
     {
+        /// <summary>
+        /// 0 for success
+        /// </summary>
         public int ErrorNo { get; set; }
         public string Message { get; set; }
+        /// <summary>
+        /// id of created record, if necessary
+        /// </summary>
         public long? Identity { get; set; }
     }
+    [RoutePrefix("api/Intervals")]
     public class IntervalsController : ApiController
     {
+        [Route("QueryIntervals")]
+        public List<TestResult> Get(DateTime DateFrom, DateTime? DateTo, string AuthId)
+        {
+            return new List<TestResult>();
+        }
         
 
         // POST api/intervals
         public ApiResult Post([FromBody]TestResult value)
         {
+
+            //todo save this object
             return new ApiResult
             {
+                ErrorNo = 0,
                 Message = "Success"
             };
         }
